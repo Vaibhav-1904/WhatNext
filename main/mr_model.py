63421462c1384f97828c1927b70065cc25c00795
unknown <vaibhav6644@gmail.com>
2021-11-22 11:23:35 +0530

    Final Changes

--- main/mr_model.py
+++ main/mr_model.py
@@ -4,16 +4,20 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from imdb import IMDb
+from .models import RecommendedMovies
 
 ###### helper functions. Use them when needed #######
 
 def get_title_from_index(index):
-	df = pd.read_csv('main/movie_dataset_IMDB.csv')
+	df = pd.read_csv('main/movie_dataset_IMDB.csv', low_memory=False)
 	return df[df.index == index]["title"].values[0]
 
 def get_index_from_title(title):
-	df = pd.read_csv('main/movie_dataset_IMDB.csv')
-	return int(df[df.title == title]["index"].values[0])
+	df = pd.read_csv('main/movie_dataset_IMDB.csv', low_memory=False)
+	try:
+		return int(df[df.title == title]["index"].values[0])
+	except:
+		return 100000000
 
 def combine_features(row):
 	try:
@@ -21,8 +25,36 @@ def combine_features(row):
 	except:
 		print("Error : ", row)
 
-def create_model():
-	df = pd.read_csv('main/movie_dataset_IMDB.csv')
+# def create_model():
+# 	df = pd.read_csv('main/movie_dataset_IMDB.csv', low_memory=False)
+# 	features = ['genres', 'cast', 'director', 'keywords']
+
+# 	for i in features:
+# 		df[i] = df[i].fillna('')
+
+# 	df["combined_features"] = df.apply(combine_features, axis=1)
+
+# 	cv = CountVectorizer()
+# 	count_matrix = cv.fit_transform(df["combined_features"])
+
+# 	cosine_simi = cosine_similarity(count_matrix)
+
+# 	return (cosine_simi, df)
+
+
+def getRecommendations(name):
+
+	ia = IMDb()
+	search = ia.search_movie(name) # create a case if no movies are found(search is empty)
+	movie = str(search[0])
+	index = get_index_from_title(movie)
+	if index == 100000000:
+		u = "https://i.redd.it/ds1luav7dl851.jpg"
+		recommended_movies = RecommendedMovies(title = "Movie Does not exist in our database", url = u)
+		recommended_movies.save()
+		return
+
+	df = pd.read_csv('main/movie_dataset_IMDB.csv', low_memory=False)
 	features = ['genres', 'cast', 'director', 'keywords']
 
 	for i in features:
@@ -32,25 +64,10 @@ def create_model():
 
 	cv = CountVectorizer()
 	count_matrix = cv.fit_transform(df["combined_features"])
-
 	cosine_simi = cosine_similarity(count_matrix)
-
-	return (cosine_simi, df)
-
-
-def getRecommendations(name):
-	ia = IMDb()
-	search = ia.search_movie(name)
-	movie_index = search[0].movieID
-	movie = ia.get_movie(movie_index)
-
-	# movies = ia.search_movie(name)
-	# movie = movies[0]
-	# index = get_index_from_title(movie.title)
-
-	df, cosine_simi = create_model()
-	similar_movies = list(enumerate(cosine_simi[movie_index]))
+	similar_movies = list(enumerate(cosine_simi[index]))
 	sorted_similar_movies = sorted(similar_movies, key=lambda x:x[1], reverse = True)# lambda x[1] specifies the similarity value in 'similar_movies'
+
 
 	j = 0
 	for i in sorted_similar_movies:
@@ -61,14 +78,19 @@ def getRecommendations(name):
 		movie_index = search[0].movieID
 		movie = ia.get_movie(movie_index)
 
-		print(movie['title'])
-		print(movie['cover url'])
+		recommended_movies = RecommendedMovies(title = movie['title'], url = movie['cover url'])
+		recommended_movies.save()
+
+		# list_of_movies.append(movie['title'])
+		# list_of_urls.append(movie['cover url'])
+		# print(movie['title'])
+		# print(movie['cover url'])
 
 		j = j + 1
-		if j > 10:
+		if j > 7:
 			break
 
-	return sorted_similar_movies
+	# return (list_of_movies, list_of_urls)
 
 	# i = 0
 	# for element in sorted_similar_movies:
--- main/views.py
+++ main/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render
 from .models import UserMovie
+from .mr_model import getRecommendations
+from .models import RecommendedMovies
 
 def homePage(request):
+
+
+
     return render(request, "main/home.html")
 
 def searchMovie(request):
 
-    movie_name = request.POST.get('name', None)
-    if movie_name != None:
-        search_movie = UserMovie(name = movie_name)
-        search_movie.save()
+    if request.method == "POST":
+        movie_name = request.POST.get('name', None)
 
-    return render(request, "main/movies.html")
+        if movie_name != None:
+
+            if UserMovie.objects.count() != 0:
+                UserMovie.objects.all().delete()
+
+            search_movie = UserMovie(name = movie_name)
+            search_movie.save()
+
+            if RecommendedMovies.objects.count() != 0:
+                RecommendedMovies.objects.all().delete()
+
+            getRecommendations(movie_name)
+
+            recommended_movies = RecommendedMovies.objects.all()
+
+    return render(request, "main/movies.html", {'recommended_movies': recommended_movies})
 
 def showMovies(request):
-
-    
-
     return render(request, "main/movies.html")
